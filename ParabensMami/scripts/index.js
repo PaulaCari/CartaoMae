1cffb6c3a3318a2ec8d728958751ecf99a0e7db1
PaulaCari <166225580+PaulaCari@users.noreply.github.com>
2024-05-04 09:56:55 -0300

    Cartão Aniversario

---
+++ ParabensMami/scripts/index.js
@@ -0,0 +1,27 @@
+alert('Buenos dias!!!')  
+
+
+function exibeAlert(){
+    alert("Apretar en la imagem para ver su contenido")
+}
+//const mensagem = "Apretar en la imagem para ver su contenido"
+//alert(mensagem)  
+
+   
+
+    
+     
+  
+ 
+
+   
+
+   
+
+   
+
+   
+   
+
+
+
